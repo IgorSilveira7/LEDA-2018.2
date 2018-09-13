b8101424a4bdf07ec838dc000fe6f94d0d7add0a
Igor Silveira <igor.andrade@ccc.ufcg.edu.br>
2018-09-13 12:02:37 -0300

    roteiro 3 add

---
+++ R03-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -0,0 +1,73 @@
+package sorting.linearSorting;
+
+import sorting.AbstractSorting;
+
+/**
+ * Classe que implementa do Counting Sort vista em sala. Desta vez este
+ * algoritmo deve satisfazer os seguitnes requisitos: - Alocar o tamanho minimo
+ * possivel para o array de contadores (C) - Ser capaz de ordenar arrays
+ * contendo numeros negativos
+ */
+public class ExtendedCountingSort extends AbstractSorting<Integer> {
+
+	@Override
+	public void sort(Integer[] array, int leftIndex, int rightIndex) {
+		
+		if(rightIndex - leftIndex > 0) {
+		
+			int max = encontraMax(array, leftIndex, rightIndex);
+			int min = encontraMin(array, leftIndex, rightIndex);
+			
+			int tamanho = max - min + 1;
+			
+			int[] aux = new int[tamanho];
+			Integer[] result = new Integer[array.length];
+			
+			int offset = min;
+			
+			for(int i = 0; i < array.length;i++) {
+				aux[array[i] - offset]++;
+			}
+			
+			for(int j = 1; j < aux.length; j++) {
+				aux[j] += aux[j - 1];
+			}
+			
+			for(int k = array.length - 1; k >= 0; k--) {
+				aux[array[k] - offset]--;
+				result[aux[array[k] - offset]] = array[k]; 
+			}
+			
+			System.arraycopy(result, leftIndex, array, leftIndex, result.length);
+			
+		}
+		
+	}
+	
+	private int encontraMax(Integer[] array, int leftIndex, int rightIndex) {
+		int max = 0;
+		
+		for (int i = leftIndex; i <= rightIndex; i++) {
+			if(array[i] > max) {
+				max = array[i];
+			}
+		}
+		
+		return max;
+	}
+	
+	private int encontraMin(Integer[] array, int leftIndex, int rightIndex) {
+		int min = array[leftIndex];
+		
+		for(int i = 0; i <= rightIndex; i++) {
+			if(array[i] < min) {
+				min = array[i];
+			}
+		}
+		
+		return min;
+	}
+	
+	
+
+}
